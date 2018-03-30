60ab0a9349f5ed941c3c60235fd7285ee9067e34
pxgamer <owzie123@gmail.com>
2018-03-30 15:17:05 +0100

    Add initial class for ChangeNow constants

---
+++ src/ChangeNow.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace pxgamer\ChangeNow;
+
+/**
+ * Class ChangeNow
+ */
+class ChangeNow
+{
+    /**
+     * The base URI for the ChangeNow main site.
+     */
+    public const BASE_URI = 'https://changenow.io';
+    /**
+     * The base URI for the ChangeNow API.
+     */
+    public const API_BASE_URI = 'https://changenow.io/api/v1';
+}
